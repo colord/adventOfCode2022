02bba4f9894979b41ed7950fa1360ec5f60ffa58
colord <10601293+colord@users.noreply.github.com>
2022-12-01 14:46:18 -0800

    Day 1 solution

--- build/day1.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-console.log("Advent of Code - Day 1!");
-const test = 4;
-let test2 = "hi";
-let test3 = "hello";
-let test4 = "hellooo";
---
+++ src/day1/day1.ts
@@ -0,0 +1,41 @@
+// Day 1
+// Part 1 - Find the elf carrying the most calories
+// Part 2 - How many calories are the top three elves carrying in total?
+
+import fs from 'fs';
+import path from 'path';
+import readline from 'readline';
+
+const dir = "./src/day1/";
+const filename = "day1-input.txt";
+
+const text = readline.createInterface({
+    input: fs.createReadStream(path.join(dir, filename))
+});
+
+let elfCalories: number[] = [];
+let calorieSum = 0;
+
+text.on('line', line => {
+    if (line === '') {
+        elfCalories.push(calorieSum);
+        calorieSum = 0;
+    }
+    else {
+        calorieSum += parseInt(line);
+    }
+});
+text.on('close', () => {
+
+    elfCalories.sort().reverse();
+    const topCalories = elfCalories[0];
+
+    const getTopThreeCalorieTotal = () => {
+        return elfCalories
+            .slice(0, 3)
+            .reduce((partialSum, val) => partialSum += val);
+    }
+
+    console.log(`❄️  ~ The elf with the most calories🍕 has ${topCalories} ~ ☃️`);
+    console.log(`❄️  ~ The top three elves are carrrying a total of ${getTopThreeCalorieTotal()} calories🍕 ~ ☃️`);
+});
